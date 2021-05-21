3fc5adb22dac2b47808aa47cb349723331ef431d
Agustin Rambaldo <agustin.rc97@gmail.com>
2021-05-21 18:37:31 -0300

    Initial commit

---
+++ Assets/EndGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject gameOverText;
+    public GameObject restartButton;
+
+    void Start()
+    {
+        gameOverText.SetActive(false);
+        restartButton.SetActive(false);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Enemy"))
+        {
+            gameOverText.SetActive(true);
+            restartButton.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public int damage = 25;
+    public float moveSpeed;
+    public Transform flag;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = flag.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+      if (col.gameObject.tag.Equals("Bullet"))
+      {
+          TakeDamage(damage);
+      }
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
